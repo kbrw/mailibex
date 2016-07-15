4dab7c9ed441e9f2a095c671983bf70539495785
heri16 <heri16@gmail.com>
2016-07-15 19:56:52 +0700

    Fix piping into a function call without parentheses,
        foo 1 |> bar 2 |> baz 3
    
    Should be written as:
    
        foo(1) |> bar(2) |> baz(3)

--- lib/dmarc.ex
+++ lib/dmarc.ex
@@ -16,8 +16,8 @@ defmodule DMARC do
        _,["!"<>_|_] -> false                                           # 
        x,y -> length(x) > length(y)                                    # else priority to longest prefix match 
      end)
-  |> Enum.each fn spec->
-    org_match = (spec|>Enum.reverse|>Enum.map fn
+  |> Enum.each (fn spec->
+    org_match = (spec|>Enum.reverse|>Enum.map (fn
       "!"<>rest->rest  # remove exception mark ! 
       "*"->quote do: _ # "*" component matches anything, so convert it to "_"
       x -> x           # match other components as they are
@@ -25,7 +25,7 @@ defmodule DMARC do
     org_len = length(spec) + 1      # and 3+1=4 first components is organization
     def organization(unquote(org_match)=host), do:
       (host |> Enum.take(unquote(org_len)) |> Enum.reverse |> Enum.join("."))
-  end
+  end)
 
   def organization([unknown_tld,org|_]), do:
     "#{org}.#{unknown_tld}"
